98306b478cd085df411bddff0a74a6a1676dd319
Anderson Ribeiro <andersonribeiro.si@hotmail.com>
2019-02-21 22:55:10 -0300

    Projeto Spring Boot criado

---
+++ src/main/java/com/andersonribeiro/cursomc/CursospringbootmcApplication.java
@@ -0,0 +1,13 @@
+package com.andersonribeiro.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringbootmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringbootmcApplication.class, args);
+	}
+
+}
